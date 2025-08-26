ad87713c3cbd037a0a8633c8a6fa4372cc23728e
Dan Atanasov <danmanm@gmail.com>
2025-08-26 16:09:20 +0300

    Enhance Books page layout and content display with improved styling and additional book details

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -10,15 +10,16 @@ export default async function Books() {
       <h1>
         <GiBookCover className="inline" /> Книги
       </h1>
-      <div className="flex gap-4">
+      <div className="flex flex-col sm:flex-row gap-4">
         {books.map((book) => (
           <Link
             href={`/books/${book.id}`}
             key={book.id}
-            className="relative w-1/2 md:w-1/3 flex items-center justify-center h-24 border-2 border-border shadow-md"
-            style={{ borderRadius: '3em 0.5em 0.5em 0' }}
+            className="relative w-full sm:w-1/2 md:w-1/3 flex flex-col items-center justify-center border-2 border-border shadow-md rounded-2xl p-4 hover:shadow-lg hover:border-primary transition active:text-background active:bg-primary"
           >
-            {book.title}
+            <div className="text-xl mb-4">{book.title}</div>
+            <div className="text-sm">Оригинал: {book.originalReleaseYear}</div>
+            <div className="text-sm">Преработка: {book.yearOfRewrite}</div>
           </Link>
         ))}
       </div>
