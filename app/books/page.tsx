549c9057578e6e6d83e5b646485a9cd6a1023058
Dan Atanasov <danmanm@gmail.com>
2025-08-26 10:29:46 +0300

    Refactor book page layout and implement Prisma client for database access

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,12 +1,27 @@
-export default function Page() {
+import prisma from '@/lib/prisma'
+import Link from 'next/link'
+import { GiBookCover } from 'react-icons/gi'
+
+export default async function Books() {
+  const books = await prisma.book.findMany()
+
   return (
     <article>
-      <p>
-        It should be stated that the game tutorials do an decent job at
-        explaining the game ins and outs. But if for one reason or another they
-        do not work for you, or you are interested in community terms that are
-        not in there, then these pages are for you.
-      </p>
+      <h1>
+        <GiBookCover className="inline" /> Книги
+      </h1>
+      <div className="flex gap-4">
+        {books.map((book) => (
+          <Link
+            href={`/books/${book.id}`}
+            key={book.id}
+            className="relative w-1/2 md:w-1/3 flex items-center justify-center h-24 border-2 border-border shadow-md"
+            style={{ borderRadius: '3em 0.5em 0.5em 0' }}
+          >
+            {book.title}
+          </Link>
+        ))}
+      </div>
     </article>
   )
 }
