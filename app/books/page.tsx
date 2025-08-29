94fc002b5ec02ea4ef514a1f22c4b6883eee983f
Dan Atanasov <danmanm@gmail.com>
2025-08-29 13:45:26 +0300

    Add getYearRange utility and update Books page to display chapter year range

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,27 +1,39 @@
 import prisma from '@/lib/prisma'
 import Link from 'next/link'
 import { GiBookCover } from 'react-icons/gi'
+import { getYearRange } from '@/app/utils'
 
 export default async function Books() {
-  const books = await prisma.book.findMany()
+  const books = await prisma.book.findMany({
+    include: {
+      chapters: true,
+    },
+  })
 
   return (
     <article>
-      <h1>
+      <h1 className="text-center">
         <GiBookCover className="inline" /> Книги
       </h1>
       <div className="flex flex-col sm:flex-row gap-4">
-        {books.map((book) => (
-          <Link
-            href={`/books/${book.id}`}
-            key={book.id}
-            className="relative w-full sm:w-1/2 md:w-1/3 flex flex-col items-center justify-center border-2 border-border shadow-md rounded-2xl p-4 hover:shadow-lg hover:border-primary transition active:text-background active:bg-primary"
-          >
-            <div className="text-xl mb-4">{book.title}</div>
-            <div className="text-sm">Оригинал: {book.originalReleaseYear}</div>
-            <div className="text-sm">Преработка: {book.yearOfRewrite}</div>
-          </Link>
-        ))}
+        {books.map((book) => {
+          const yearRange = getYearRange(book.chapters)
+
+          return (
+            <Link
+              href={`/books/${book.id}`}
+              key={book.id}
+              className="relative w-full sm:w-1/2 md:w-1/3 flex flex-col items-center justify-center border-2 border-border shadow-md rounded-2xl p-4 hover:shadow-lg hover:border-primary transition active:text-background active:bg-primary text-center"
+            >
+              <h2>{book.title}</h2>
+              <div className="text-lg">{book.series.join(', ')}</div>
+
+              <div>
+                {book.chapters.length} глави - {yearRange}
+              </div>
+            </Link>
+          )
+        })}
       </div>
     </article>
   )
