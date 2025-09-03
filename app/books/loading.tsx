7b0c74995c42c42da27ae9957cdb49bc7072b115
Dan Atanasov <danmanm@gmail.com>
2025-09-03 14:52:42 +0300

    Replace Spinner component with GiYinYang icon in Loading component

--- app/books/loading.tsx
+++ app/books/loading.tsx
@@ -1,5 +1,9 @@
-import { Spinner } from '@/app/ui/Spinner'
+import { GiYinYang } from 'react-icons/gi'
 
 export default function Loading() {
-  return <Spinner />
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center text-9xl">
+      <GiYinYang className="animate-spin mr-4" />
+    </div>
+  )
 }
--- app/ui/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { GiYinYang } from 'react-icons/gi'
-
-export const Spinner = ({ message }: { message?: string }) => {
-  return (
-    <div className="min-h-screen w-full flex items-center justify-center text-3xl">
-      <GiYinYang className="animate-spin mr-4" /> {message || 'Миг...'}
-    </div>
-  )
-}
