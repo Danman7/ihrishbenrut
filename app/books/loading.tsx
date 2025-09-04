15dba86ca2ac84345447f9a13f2cdcc256441a3c
Dan Atanasov <danmanm@gmail.com>
2025-09-04 14:25:15 +0300

    Loader doesn't push the footer

--- app/books/loading.tsx
+++ app/books/loading.tsx
@@ -2,7 +2,7 @@ import { GiYinYang } from 'react-icons/gi'
 
 export default function Loading() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center text-9xl">
+    <div className="w-full h-full flex items-center justify-center text-9xl flex-grow">
       <GiYinYang className="animate-spin mr-4" />
     </div>
   )
